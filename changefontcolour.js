953ebb990f6244c4a833f3b7409d0f2f8a927963
chmalathi <31770843+chmalathi@users.noreply.github.com>
2017-10-05 23:52:05 -0400

    Add files via upload

---
+++ changefontcolour.js
@@ -0,0 +1,7 @@
+ function changefont()
+            {
+
+                var x = document.getElementById("cn");
+                x.style.fontSize = "20px";           
+                x.style.color = "red"; 
+            }
